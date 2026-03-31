6207697b74edda62f18ac92dd078658c7b7fcfea
Gowtham <gowthampalanivel0420@gmail.com>
2026-03-31 09:12:30 +0530

    Create sensors.c

---
+++ include/sensors.c
@@ -0,0 +1,18 @@
+1. config.h - header files
+#ifndef CONFIG_H
+#define CONFIG_H
+
+// WiFi
+#define WIFI_SSID "YOUR_WIFI"
+#define WIFI_PASS "YOUR_PASSWORD"
+
+// Blynk
+#define BLYNK_AUTH_TOKEN "YOUR_TOKEN"
+
+// Pins
+#define DHTPIN 4
+#define GAS_PIN 27
+#define NOISE_PIN 18
+#define BUZZER 26
+
+#endif
